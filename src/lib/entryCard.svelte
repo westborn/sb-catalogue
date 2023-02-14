e3150806992553a65ff71abf2661a145ad1f6260
George Stone <george@westborn.com.au>
2023-02-14 21:17:15 +1100

    format card to maintain exhibit circle background

--- src/lib/entryCard.svelte
+++ src/lib/entryCard.svelte
@@ -18,25 +18,21 @@
 
 <!-- card -->
 <div class="relative flex flex-col items-center justify-between rounded-xl border-2 bg-blue-50">
-	<div class="flex w-full items-center p-3">
-		<div
-			class="flex h-10 w-10 items-center justify-center rounded-full bg-blue-500 font-bold text-white"
-		>
+	<div class="flex w-full items-center py-3 px-2">
+		<div class="flex items-center justify-center rounded-full bg-blue-500 p-4 font-bold text-white">
 			<p>{entryNumber}</p>
 		</div>
-		<span class="ml-2 pt-1 text-sm font-bold" loading="lazy" decoding="async"
-			>{title} - {artistName}</span
-		>
+		<span class="ml-2 pt-1 text-sm font-bold">{title} - {artistName}</span>
 	</div>
 	<img
-		class="max-h-96 rounded-2xl p-2"
+		class="max-h-96 rounded-2xl px-2"
 		src={`${websiteURL}/${imageId}.${imageExtension}`}
 		on:error={handleError}
 		alt={imageFileName}
 	/>
 	<div class="w-full px-3 pb-2">
 		<p class="text-base">{description}</p>
-		<p class="text-xs">{material}</p>
+		<p class="mt-1 text-center text-xs">{material}</p>
 		<div class="flex flex-row justify-between">
 			<p class="text-xs">{size}</p>
 			<p class="text-xs">{inOrOut}</p>
